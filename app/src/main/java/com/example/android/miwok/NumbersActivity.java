083ba52a9f6cbace4d2f2adc5d11e5f1ea7e9856
Dawn Tan <dawntan08@gmail.com>
2017-03-30 21:03:59 +1100

    - modify activity_numbers.xml     - change to Linearlayout - add arraylist in numbersactivity    - add words    - add words to textview

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //list of words to be added
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // find root view of the layout
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        // create textView and loop to add all elements in the arraylist
+        for(int index = 0; index < 10; index++){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+        }
+
     }
 }
