49742fde10a8b7e4fb3c49fb9dfdc0bfa7b5b6ec
Dawn Tan <dawntan08@gmail.com>
2017-04-08 23:31:52 +1000

    - modify WordAdapter to set imageview to correct image - add checking if resource id is provided for image i.e. for numbers, colors, family so that phrases wont have an image

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -40,6 +41,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
         defaultTextView.setText(currentWord.getDefaultTranslation());
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text);
         miwokTextView.setText(currentWord.getMiwokTranslation());
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        if(currentWord.getImageResourceId() != 0){
+            imageView.setImageResource(currentWord.getImageResourceId());
+        } else{
+            imageView.setVisibility(View.GONE);
+        }
+
 
         return listItemView;
     }
