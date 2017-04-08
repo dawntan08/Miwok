7690fdfcd371f52ce18cd3f877c1ca3c5da65f07
Dawn Tan <dawntan08@gmail.com>
2017-04-08 16:36:32 +1000

    - refactor activity_numbers.xml to word_list.xml to be used by all activities except main - add Words for colors, family and phrases
    Clicking Colors, Family, Numbers, and Phrases in the main activity should now show pairs of Miwok and English word translations.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,12 +2,33 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        //list of words, default and miwok translation to be added
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother ", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        // populate listView using custom WordAdapter and custom list_item.xml
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,6 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,7 +11,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         //list of words, default and miwok translation to be added
         ArrayList<Word> words = new ArrayList<Word>();
@@ -31,6 +30,5 @@ public class NumbersActivity extends AppCompatActivity {
         WordAdapter adapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
-
     }
 }
